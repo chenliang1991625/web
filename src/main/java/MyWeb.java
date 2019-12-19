a6db2b05a89a46ba0a7092ff16a5f982a848ad8c
chenliang <1484157619@qq.com>
2019-12-19 14:08:36 +0800

    package war

---
+++ src/main/java/MyWeb.java
@@ -0,0 +1,9 @@
+public class MyWeb {
+    private static String webName="web我来了!";
+    public static void main(String[] args) {
+        System.out.println("您好!恭喜您成功了!!!");
+    }
+    public String sentWeb(){
+        return webName;
+    }
+}
